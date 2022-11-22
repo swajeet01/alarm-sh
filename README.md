885c98f7508af57fd77b3fdda53ff842a3343d8d
swajeet01 <swajeetg768@gmail.com>
2022-11-22 19:52:14 +0530

    Add a bit of information to README.md

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # alarm-sh
-Basic terminal alarm utility
+Basic terminal alarm utility, rings terminal bell and prints short message.
+
+Prints usage if script is executed without any arguments.
+
--- alarm.sh
+++ alarm.sh
@@ -6,9 +6,8 @@ AUTHOR="Swajeet Swarnkar"
 DONE_MSG="It's $1!"
 
 usage() {
-    echo "Basic alarm utility, version $VERSION, author $AUTHOR"
+    echo "Basic terminal alarm utility, version $VERSION, author $AUTHOR."
     echo "Usage:    $0 <time>"
-    echo
     echo "Arguments-"
     echo "<time>    time in hh:mm format (24 hr)"
 }
@@ -17,13 +16,17 @@ if [[ $# -ne 1 ]] then
     usage
     exit 64
 fi
-    
+
+echo "Alarm set for $1."
+
 while [[ $(date "+%H:%M") != $1 ]] do
     sleep 5
 done
 
+# Rings terminal bell
 tput bel
 
+# Uncomment if you want `wall` message
 # wall $DONE_MSG
 
 echo $DONE_MSG
