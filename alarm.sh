9b0c79fe0178c86144ad318fe50866f858900a85
swajeet01 <swajeetg768@gmail.com>
2022-12-19 12:08:49 +0530

    Add time validation.

--- alarm.sh
+++ alarm.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-VERSION="1.0.0"
+VERSION="1.0.1"
 AUTHOR="Swajeet Swarnkar"
 
+# Modify message shown when alarm goes off.
 DONE_MSG="It's $1!"
 
 usage() {
@@ -12,14 +13,13 @@ usage() {
     echo "<time>    time in hh:mm format (24 hr)"
 }
 
-if [[ $# -ne 1 ]] then
-    usage
-    exit 64
-fi
+[[ $# -ne 1 ]] && usage && exit 64
+
+[[ ! $1 =~ ^([01][0-9]|2[0-3]):?[0-5][0-9]$ ]] && echo "Bad time parameter \"$1\"" && exit 64
 
 echo "Alarm set for $1."
 
-while [[ $(date "+%H:%M") != $1 ]] do
+while [[ $(date "+%H:%M") != $1 ]]; do
     sleep 5
 done
 
